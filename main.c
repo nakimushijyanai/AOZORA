dc1b76741162b765a5fa07cdd044739b4371839a
nakimushijyanai <nakimushijyanaiforever@gmail.com>
2024-05-07 18:17:50 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main() {
    // printf() displays the string inside quotation
+	printf("World, Hello!");
    printf("Hello, World!");
    printf("Hello, World!");
    return 0;
